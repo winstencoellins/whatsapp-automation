3a508c4480c64b3b9f32bcc0833ea238b1c817bd
Winsten Coellins <winsten@umify.io>
2025-04-12 23:22:42 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pywhatkit
+import pyautogui as pg
+import time
+import pandas as pd
+
+# df = pd.read_excel("./data/recipient.xlsx")
+
+# recipient = df['Nomor Telepon'].to_numpy()
+
+# for i in recipient:
+#     print(i)
+
+test = ["+6287829112648", "+628116359119"]
+
+msg = "Anda diundang untuk menghadiri acara graduation MSJ GMS."
+
+try:
+    for i in test:
+        pywhatkit.sendwhatmsg_instantly(i, msg, 15, True, 5)
+
+        time.sleep(1)
+        pg.leftClick()
+
+        pg.press("enter")
+except:
+    print("Internet connection error.")
